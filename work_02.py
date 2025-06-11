5000ef8640f6e4f9a3d20a8a1dff6fc82d9d0ca8
Sanshiro-Yamashita <sansilangshanxia@gmail.com>
2025-06-11 12:17:27 +0900

    反射神経テスト

--- work_02.py
+++ work_02.py
@@ -1,18 +1,25 @@
 import time
 import random 
 import math
-n = random.randint(5,15)
-print("反射神経テストを始めます。　! ! ! ! !　と表示されたらEnterを押して下さい。")
-time.sleep(n)
-print("! ! ! ! !")
-start_time = time.time()
-input()
-end_time = time.time()
-k = end_time - start_time
-k = math.floor(k * 10000) / 10000
-if k < 0.01:
-    print("早すぎです。連打などは不正になります。")
-else:
-    print(f"あなたの反応速度は{k}秒です。")
-
-
+while True:
+    li = []
+    n = random.randint(5,15)
+    print("反射神経テストを始めます。　! ! ! ! !　と表示されたらEnterを押して下さい。")
+    time.sleep(n)
+    print("! ! ! ! !")
+    start_time = time.time()
+    input()
+    end_time = time.time()
+    k = end_time - start_time
+    k = math.floor(k * 10000) / 10000
+    li.append(k)
+    if k < 0.01:
+        print("早すぎです。連打などは不正になります。")
+    else:
+        print(f"あなたの反応速度は{k}秒です。")
+    d = input("もう1度プレイする場合は'yes'を終了する場合は'no'を入力してください")
+    if d == "no":
+        z = min(li)
+        print(f"最高記録は{z}秒でした。")
+        print("終わります。ありがとうございました!")
+        break
